f066fcb643141fed65acd52ac4de69d6c98c2ade
alihangudenoglu <alihan_135@hotmail.com>
2024-05-06 16:44:13 +0300

    Section 3 is completely

--- src/RentACar/WebApi/Program.cs
+++ src/RentACar/WebApi/Program.cs
@@ -9,8 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceService(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddStackExchangeRedisCache(opt=>opt.Configuration="localhost:6379");
+builder.Services.AddStackExchangeRedisCache(opt=>opt.Configuration= "localhost:1453");
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
